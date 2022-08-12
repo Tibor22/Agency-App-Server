97b06386b725890054a5b4aca84a87feee738abd
Tibor22 <tothtibitravel@gmail.com>
2022-08-12 23:14:14 +0100

    added react build

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ import cors from 'cors';
 import morgan from 'morgan';
 import 'dotenv/config';
 import api from './routes/api.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(express.static('images'));
@@ -13,6 +17,12 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use('/v1', api);
 app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.get('*', (req, res) => {
 	res.status(404).json({
